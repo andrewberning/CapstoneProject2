c7f85c3e82b47bc29a58615ce56d1427e31fb401
andrewberning <berningan89@gmail.com>
2024-07-07 16:54:16 -0700

    Created get user route

---
+++ server/routes/users.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/** Routes for users. */
+
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
+const { BadRequestError } = require("../expressError");
+const User = require("../models/user");
+const { createToken } = require("../helpers/tokens");
+
+
+const router = express.Router();
+
+
+/** GET /[username] => { user }
+ *
+ * Returns { username, firstName, lastName, isAdmin, jobs }
+ *   where jobs is { id, title, companyHandle, companyName, state }
+ *
+ * Authorization required: admin or same user-as-:username
+ **/
+
+router.get("/:username", async function (req, res, next) {
+  try {
+    const user = await User.get(req.params.username);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
