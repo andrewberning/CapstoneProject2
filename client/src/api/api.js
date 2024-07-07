293980e757d44d71915f2061baf002e7af7dce30
andrewberning <berningan89@gmail.com>
2024-07-07 16:13:43 -0700

    Added getCurrentUser and login static methods

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const BASE_API_URL = "http://localhost:3000";
 
 class ShoplyApi {
+  // the token for interactive with the API will be stored here.
+  static token;
 
   static async getCategories() {
     const result = await axios.get(`${BASE_API_URL}/categories`);
@@ -17,6 +19,17 @@ class ShoplyApi {
   static async getProduct(id, category) {
     const result = await axios.get(`${BASE_API_URL}/${category}/${id}`);
     return result.data;
+  }
+
+  static async getCurrentUser(username) {
+    const result = await axios.get(`${BASE_API_URL}/users/${username}`);
+    console.log("result for getCurrentUser is: ", result.data.user);
+    return result.data.user;
+  }
+
+  static async login(data) {
+    const result = await axios.post(`${BASE_API_URL}/auth/token`, data);
+    return result.data.token;
   }
 
 }
