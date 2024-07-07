38cb33caf6d0a2a26ecbf40f091bca93737e6423
andrewberning <berningan89@gmail.com>
2024-07-07 16:30:40 -0700

    Modified currentUser ternary orperation

--- client/src/banner/WelcomeBanner.jsx
+++ client/src/banner/WelcomeBanner.jsx
@@ -9,10 +9,8 @@ export default function WelcomeBanner() {
     <section className="WelcomeBanner d-flex flex-column justify-content-center align-items-center">
       <div className="text-center ">
         {currentUser
-          ? <h2>Welcome Back, {currentUser}!</h2>
-          : (
-              <h2>Welcome to Shoply</h2>
-            )
+          ? <h2>Welcome Back, {currentUser.firstName}!</h2>
+          : <h2>Welcome to Shoply</h2>
         }
         <p className="lead">Everything in one, convenient place.</p>
       </div>
