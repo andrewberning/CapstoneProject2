5b3f144137811cb9be2bc658a9e185198de06404
andrewberning <berningan89@gmail.com>
2024-07-07 17:26:22 -0700

    Changed authenticateJWT middleware variable name

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const { NotFoundError } = require("./expressError");
 
-const { authenticateJWT } = require("./middleware/auth");
+const authenticateJWT = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const categoriesRoutes = require("./routes/categories");
 const usersRoutes = require("./routes/users");
