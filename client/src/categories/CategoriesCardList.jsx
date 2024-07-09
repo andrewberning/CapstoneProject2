14486f0c08485d3af2a4458970112480a7e3d53d
andrewberning <berningan89@gmail.com>
2024-07-08 18:03:43 -0700

    Added image prop for CategoryCard component

--- client/src/categories/CategoriesCardList.jsx
+++ client/src/categories/CategoriesCardList.jsx
@@ -1,12 +1,14 @@
-import CategoriesCard from "./CategoriesCard"
+import CategoryCard from "./CategoryCard"
 
 export default function CategoriesCardList({ categories }) {
   return (
     <div className="ClothingCardList d-flex justify-content-center gap-4">
       {categories.map(category => (
-        <CategoriesCard
+        <CategoryCard
           key={category.id}
+          id={category.id}
           category={category.name}
+          image={category.image_url}
         />
       ))}
     </div>
