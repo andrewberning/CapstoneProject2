85c8aa4c0505317d4a7973109fb56066187be1b8
andrewberning <berningan89@gmail.com>
2024-07-08 18:02:03 -0700

    Modified useEffect

--- client/src/banner/CategoriesBanner.jsx
+++ client/src/banner/CategoriesBanner.jsx
@@ -8,9 +8,9 @@ export default function CategoriesBanner() {
   const [categories, setCategories] = useState([]);
 
   useEffect(function getFiveCategoriesOnMount() {
-    async function getCategories(category) {
-      let categories = await ShoplyApi.getCategories(category);
-      setCategories(categories);
+    async function getCategories() {
+      let categories = await ShoplyApi.getCategories();
+      setCategories(categories.slice(0,5));
     }
 
     getCategories();
@@ -22,7 +22,7 @@ export default function CategoriesBanner() {
           <h2 className="mb-4 font-weight-bold">Featured Categories</h2>
           <Link to={"/categories"}>See all</Link>
         </div>
-        <CategoriesCardList categories={categories.slice(0,5)} />
+        <CategoriesCardList categories={categories} />
 
     </section>
   );
