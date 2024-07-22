a6f88b5aa2b7b060146885465550250eec3e87b3
andrewberning <berningan89@gmail.com>
2024-07-22 16:43:14 -0600

    Removed unnecessary hooks, state, and props to CategoriesCardList

--- client/src/banner/CategoriesBanner.jsx
+++ client/src/banner/CategoriesBanner.jsx
@@ -1,20 +1,8 @@
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import CategoriesCardList from "../categories/CategoriesCardList";
-import ShoplyApi from '../api/api';
+import CategoriesBannerList from "../banner/CategoriesBannerList";
 import "./CategoriesBanner.css";
 
 export default function CategoriesBanner() {
-  const [categories, setCategories] = useState([]);
-
-  useEffect(function getFiveCategoriesOnMount() {
-    async function getCategories() {
-      let categories = await ShoplyApi.getCategories();
-      setCategories(categories.slice(0,5));
-    }
-
-    getCategories();
-  }, [])
 
   return (
     <section className="CategoriesBanner">
@@ -22,7 +10,7 @@ export default function CategoriesBanner() {
           <h2 className="mb-4 font-weight-bold">Featured Categories</h2>
           <Link to={"/categories"}>See all</Link>
         </div>
-        <CategoriesCardList categories={categories} />
+        <CategoriesBannerList />
 
     </section>
   );
