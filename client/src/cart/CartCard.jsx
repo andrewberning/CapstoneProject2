c15ad47a5c062b9f16e5605d2f26419e926598c0
andrewberning <berningan89@gmail.com>
2024-07-22 16:54:36 -0600

    Modified CartCard to show additonal data and have proper link route

--- client/src/cart/CartCard.jsx
+++ client/src/cart/CartCard.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 import Card from "react-bootstrap/card"
 // import Button from "react-bootstrap/Button";
 
-export default function CartCard({ name, desc, price, img }) {
+export default function CartCard({ id, name, desc, price, img, quantity, categoryId }) {
+
+      // NEED TO FIND A WAY TO GET THE CATEGORY FOR LINK TO GO TO PRODUCT CARD
 
   return (
-    <Link className="CartCard card d-flex flex-row mx-1 align-items-center" to={"/"}>
+    <Link className="CartCard card d-flex flex-row mx-1 align-items-center" to={`/${categoryId}/${id}`}>
       <div className="cart-item-img">
         <img src={img} />
       </div>
@@ -17,6 +19,9 @@ export default function CartCard({ name, desc, price, img }) {
         </Card.Text>
         <Card.Text>
           {desc}
+        </Card.Text>
+        <Card.Text>
+          Qty: {quantity}
         </Card.Text>
         </div>
       </Card.Body>
