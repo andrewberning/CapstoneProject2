460d541f02e3754795a2e00635d232aaf82aa8aa
andrewberning <berningan89@gmail.com>
2024-07-22 16:56:57 -0600

    Modified component to use cartItems from UserContext

--- client/src/cart/CartList.jsx
+++ client/src/cart/CartList.jsx
@@ -1,13 +1,9 @@
+import { useContext } from 'react';
 import CartCard from './CartCard';
-const stockImg = "https://www.shutterstock.com/image-vector/lost-items-line-vector-icon-260nw-1436787446.jpg"
+import UserContext from '../auth/UserContext';
 
 export default function CartList() {
-  const cartItems = [
-    {name: "Rocking Horse", desc: "A fun Rocking Horse to play with.", price: "$9.99", img: stockImg},
-    {name: "PS5", desc: "Gaming console.", price: "$399.99", img: stockImg},
-    {name: "Adidas T-Shirt", desc: "An awesome t-shirt to wear.", price: "$13.99", img: stockImg},
-  ];
-  // const cartItems = null;
+  const { cartItems } = useContext(UserContext);
 
   // Function to calculate total price
   const calculateTotalPrice = (items) => {
@@ -31,24 +27,25 @@ export default function CartList() {
       </div>
       
       <div className='d-flex'>
-      {cartItems
-        ? (
-            <div className="cart-list-container w-50">
-              <div className="cartItemsList d-flex flex-column">
-                {cartItems.map(item => (
-                  <CartCard 
-                    key={item.name}
-                    name={item.name}
-                    desc={item.desc}
-                    price={item.price}
-                    img={item.img}
-                  />
-                ))}
-              </div>
+        {cartItems.length > 0 ? (
+          <div className="cart-list-container w-50">
+            <div className="cartItemsList d-flex flex-column">
+              {cartItems.map(item => (
+                <CartCard 
+                  key={item.product_id}
+                  id={item.product_id}
+                  name={item.name}
+                  desc={item.description}
+                  price={item.price}
+                  img={item.image_url}
+                  quantity={item.quantity}
+                  categoryId={item.category_id}
+                />
+              ))}
             </div>
-          )
-        : <h2>Your cart is empty</h2>
-      }
+          </div>
+        ) : <h2>Your cart is empty</h2>
+        }
 
       <div className="order-summary-container">
         <div className="order-summary-title">
@@ -57,9 +54,8 @@ export default function CartList() {
         <div className="order-summary-total">
           {`$${calculateTotalPrice(cartItems)}`}
         </div>
-
       </div>
-      </div>
+    </div>
     </div>
   );
 }
