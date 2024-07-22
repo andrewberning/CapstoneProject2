b0863ab6ef6f717f043b74b0e93c032aed051899
andrewberning <berningan89@gmail.com>
2024-07-22 17:04:19 -0600

    Edited spaces and removed empty lines

--- client/src/categories/CategoryCard.jsx
+++ client/src/categories/CategoryCard.jsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom";
 
-
 export default function CategoryCard({ category, image }) {
   return (
     <div className="CategoryCard card d-flex flex-column">
       <Link className="category-link" to={`/categories/${category}`}>
-      <div className="product-img-container">
-        <img src={image} className="card-img" />
-      </div>
-      <div className="card-body">
-        <div className="cart-item-details">
-          <div className="card-title">{category}</div>
+        <div className="category-img-container">
+          <img src={image} className="card-img" />
         </div>
-      </div>
+        <div className="card-body">
+          <div className="card-title text-center">{category}</div>
+        </div>
       </Link>
     </div>
   );
