c9d45855264c24adde9c42e3e0d4bb4d328371b7
andrewberning <berningan89@gmail.com>
2024-07-22 17:01:50 -0600

    Removed unnecessary hooks, state, and props passed to nested component

--- client/src/categories/CategoriesList.jsx
+++ client/src/categories/CategoriesList.jsx
@@ -1,24 +1,11 @@
-import { useState, useEffect } from "react";
-import ShoplyApi from "../api/api";
 import CategoriesCardList from "./CategoriesCardList";
 
 export default function CategoriesList() {
-  const [categories, setCategories] = useState([]);
-
-  
-  useEffect(function getAllCategoriesOnMount() {
-    async function getAllCategories() {
-      let categories = await ShoplyApi.getCategories();
-      setCategories(categories);
-    }
-
-    getAllCategories();
-  }, [])
 
   return (
-    <div className="ClothingList">
+    <div className="CategoriesList">
       <h2>Categories</h2>
-      <CategoriesCardList categories={categories} />
+      <CategoriesCardList />
     </div>
   )
 }
