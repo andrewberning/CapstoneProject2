c1b1ef01813f071523b00ca0baa03d313def649b
andrewberning <berningan89@gmail.com>
2024-08-12 13:37:04 -0500

    Modified db

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,11 @@
 /** Database setup for shoply. */
 
 const { Client } = require("pg");
-const { getDatabaseUri } = require("./config")
-// const { DB_URI } = require("./config");
+const { DB_URI } = require("./config");
 
-let DB_URI;
+const client = new Client(DB_URI);
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///jobly_test";
-} else {
-  "postgres://qtlxddvu:9AapQXd0wpq5dQCyugzefWrH3emu8stW@kala.db.elephantsql.com/qtlxddvu"
-}
-
-// const client = new Client(DB_URI);
-let db = new Client({
-  connectionString: DB_URI
-});
-
-db.connect();
+client.connect();
 
 
-module.exports = db;
+module.exports = client;
