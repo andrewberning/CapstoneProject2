194bd3fce00c1d1972b5c41a1ff100dfabf98a19
andrewberning <berningan89@gmail.com>
2024-08-12 17:15:07 -0500

    Implemented Loading Spinner

--- client/src/products/ProductsCardList.jsx
+++ client/src/products/ProductsCardList.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import ShoplyApi from "../api/api";
 import ProductCard from "./ProductCard";
 import "./ProductsCardList.css";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 export default function ProductsList() {
   const { category } = useParams();
@@ -25,29 +26,24 @@ export default function ProductsList() {
     return <p className="lead text-center">Category not found.</p>
   }
 
+  if (!products) return <LoadingSpinner />
+
   return (
     <div className="ProductsCardList">
       <h2 className="text-center">{category.charAt(0).toUpperCase() + category.slice(1)}</h2>
-      {products.length
-        ? (
-            <ul className="products-list">
-              {products.map(product => (
-                <ProductCard 
-                key={product.id}
-                id={product.id}
-                category={category}
-                name={product.name}
-                description={product.description}
-                price={product.price}
-                image={product.image_url}
-                />
-              ))}
-            </ul>
-        ) : (
-          <p className="lead text-center">Sorry, no results were found.</p>
-        )
-      }
-
+      <ul className="products-list">
+        {products.map(product => (
+          <ProductCard 
+          key={product.id}
+          id={product.id}
+          category={category}
+          name={product.name}
+          description={product.description}
+          price={product.price}
+          image={product.image_url}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
