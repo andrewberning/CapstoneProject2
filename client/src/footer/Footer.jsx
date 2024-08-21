7e235afc629684320ec17cb404c4080f539e5a18
andrewberning <berningan89@gmail.com>
2024-08-21 12:08:15 -0500

    Add footer

---
+++ client/src/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+export default function Footer() {
+  
+  return (
+    <div className="Footer">
+      <footer className="footer-container">
+        <p>SHOPLY</p>
+        <span>Copywrite 2024. A. Berning</span>
+      </footer>
+    </div>
+  );
+}
